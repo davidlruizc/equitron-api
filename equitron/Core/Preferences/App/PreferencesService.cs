5a4b7851aa5bc659c83a5100165683f7935e0976
David Ruiz <davidl-ruizc@outlook.com>
2023-05-12 19:57:12 -0500

    add preferences service app

---
+++ equitron/Core/Preferences/App/PreferencesService.cs
@@ -0,0 +1,9 @@
+﻿using Core.Preferences.Domain.Services;
+
+namespace Core.Preferences.App
+{
+    public class PreferencesService
+    {
+        private readonly IPreferencesRepository repository;
+    }
+}
