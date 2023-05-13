843bf471fa0734279cee18bf8ffe9339de25471e
David Ruiz <davidl-ruizc@outlook.com>
2023-05-12 22:49:38 -0500

    add dto

--- equitron/Core/Preferences/App/DTO/PreferencesDTO.cs
+++ equitron/Core/Preferences/App/DTO/PreferencesDTO.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Core.Preferences.App.DTO
 {
-    internal class PreferencesDTO
+    public class PreferencesDTO
     {
+        public Guid Id { get; set; }
+        public string Symbol { get; set; }
+        public Guid UserId { get; set; }
+
+        private PreferencesDTO(Guid id, string symbol, Guid userId)
+        {
+            Id = id;
+            Symbol = symbol;
+            UserId = userId;
+        }
+
+        public PreferencesDTO Of(Domain.Model.Preferences model)
+        {
+            return new PreferencesDTO(model.Id, model.Symbol, model.UserId);
+        }
     }
 }
