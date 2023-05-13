0dbc0688e7b4c49a9c5a5dcb3365f3e2b35cb2f8
David Ruiz <davidl-ruizc@outlook.com>
2023-05-12 19:22:56 -0500

    add preferences model list

--- equitron/Core/Preferences/Domain/Model/Preferences.cs
+++ equitron/Core/Preferences/Domain/Model/Preferences.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Utilities.Domain;
+﻿using Utilities.Domain;
 
 namespace Core.Preferences.Domain.Model
 {
