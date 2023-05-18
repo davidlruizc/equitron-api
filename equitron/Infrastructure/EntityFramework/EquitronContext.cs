b9275460442ca7ade380d8e65a6ca006e5f80ed3
David Ruiz <davidl-ruizc@outlook.com>
2023-05-18 18:02:16 -0500

    add infrastructure

---
+++ equitron/Infrastructure/EntityFramework/EquitronContext.cs
@@ -0,0 +1,12 @@
+﻿using Core.Users.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EntityFramework
+{
+    public partial class EquitronContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        public EquitronContext() { }
+    }
+}
