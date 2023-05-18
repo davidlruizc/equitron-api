9b66bdf3174018ab7af11c575519613fd8d022a5
David Ruiz <davidl-ruizc@outlook.com>
2023-05-18 18:33:06 -0500

    add configurations

---
+++ equitron/Infrastructure/EntityFramework/Configurations/PreferencesConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Core.Preferences.Domain.Model;
+using Core.Users.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityFramework.Configurations
+{
+    internal class PreferencesConfiguration : IEntityTypeConfiguration<Preferences>
+    {
+        public void Configure(EntityTypeBuilder<Preferences> builder)
+        {
+            builder.ToTable("preferences");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Symbol).IsRequired();
+
+            builder.Property(p => p.Creation).IsRequired();
+
+            builder.HasOne<Users>().WithMany().HasForeignKey(p => p.UserId);
+        }
+    }
+}
