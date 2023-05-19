ae34354ddf9e7e56a61118b7c6c4bd527223c845
David Ruiz <davidl-ruizc@outlook.com>
2023-05-18 20:23:26 -0500

    add users repository

--- equitron/Utilities/Repository/ICrudRepository.cs
+++ equitron/Utilities/Repository/ICrudRepository.cs
@@ -4,5 +4,9 @@
     {
         void Save<TE>(TE entity) where TE : class;
         void Delete<TE>(object entity) where TE : class;
+        TE? Find<TE>(object id) where TE : class;
+        TE FindOrFail<TE>(object id) where TE : class;
+        IList<TE> FindAll<TE>() where TE : class;
+        void CommitChanges();
     }
 }
