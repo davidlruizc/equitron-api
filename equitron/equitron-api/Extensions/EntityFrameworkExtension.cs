045181e9f63f7ff4a0a328cd8cd40776bc9f5dc5
David Ruiz <davidl-ruizc@outlook.com>
2023-05-18 21:28:34 -0500

    change dependencies and add entity framework extension

---
+++ equitron/equitron-api/Extensions/EntityFrameworkExtension.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Infrastructure.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace equitron_api.Extensions
+{
+    public static class EntityFrameworkExtension
+    {
+        public static void AddEntityFramework(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EquitronContext>(c =>
+            {
+                c.UseNpgsql(configuration.GetConnectionString("DBConnection"));
+            });
+        }
+    }
+}
