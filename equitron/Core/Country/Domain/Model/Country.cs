f0473a7f17a36c948bd797902f56eed4553d7dfe
David Ruiz <53538743+davidlruizc@users.noreply.github.com>
2023-05-31 17:07:03 -0500

    add country model

---
+++ equitron/Core/Country/Domain/Model/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Utilities.Domain;
+
+namespace Core.Country.Domain.Model
+{
+	public class Country : BaseEntity
+	{
+		public string Name { get; set; }
+		public string ShortName { get; set; }
+	}
+}
+
