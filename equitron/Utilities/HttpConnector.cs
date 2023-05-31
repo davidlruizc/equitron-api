65e30113e224c638749bd08bea0fe6870dd5552e
David Ruiz <davidl-ruizc@outlook.com>
2023-05-30 22:58:18 -0500

    add http connector

---
+++ equitron/Utilities/HttpConnector.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+using Utilities.Exceptions;
+
+namespace Utilities
+{
+    public class HttpConnector
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public HttpConnector(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T> Get<T>(string path)
+        {
+            try
+            {
+                var httpClient = httpClientFactory.CreateClient();
+                var response = await httpClient.GetAsync(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadFromJsonAsync<T>();
+                    return content;
+                }
+                else
+                {
+                    throw new CustomException(response.ReasonPhrase);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is CustomException)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw new CustomException(ex.Message);
+                }
+            }
+        }
+    }
+}
