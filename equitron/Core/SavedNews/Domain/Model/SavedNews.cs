c0b3b761f539a9765b28609d647e9363049abb10
David Ruiz <53538743+davidlruizc@users.noreply.github.com>
2023-05-31 17:07:15 -0500

    add saved news model

---
+++ equitron/Core/SavedNews/Domain/Model/SavedNews.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Utilities.Domain;
+
+namespace Core.SavedNews.Domain.Model
+{
+	public class SavedNews : BaseEntity
+	{
+		public Guid UserId { get; set; }
+		public Guid NewsId { get; set; }
+		public string Title { get; set; }
+	}
+}
+
